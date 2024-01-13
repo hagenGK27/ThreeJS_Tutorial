b2d5711988a384daccdb0256ead882746594cee9
hagen wey <hagen.wey@outlook.de>
2024-01-13 14:03:28 +0100

    implemented spinning cube with three.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { WebGL1Renderer } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,3 +14,11 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
